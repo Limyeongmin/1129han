4692e8f276c6fa8a6567c78fcb7812ca389bf150
Limyeongmin <ahkongbu@naver.com>
2016-10-18 16:22:58 +0900

    2016-10-18 로그인정의

--- app/src/main/java/com/example/lym/han1129_first/MainActivity.java
+++ app/src/main/java/com/example/lym/han1129_first/MainActivity.java
@@ -1,14 +1,65 @@
 package com.example.lym.han1129_first;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
+    Button btnLogin, btnSign;
+    EditText stdNum,pw;
+    SharedPreferences pref;
+    CheckBox autoLogin;
+    RadioButton professor, student;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        stdNum = (EditText) findViewById(R.id.emailInput);
+        pw = (EditText) findViewById(R.id.pw);
+        autoLogin = (CheckBox) findViewById(R.id.checkBox);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        professor = (RadioButton) findViewById(R.id.professor);
+        student = (RadioButton) findViewById(R.id.student);
+        btnSign = (Button) findViewById(R.id.btnSign);
+
+
+
+
+
+        //회원가입 버튼 정의
+        btnSign.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (student.isChecked()){
+                    Intent i = new Intent(MainActivity.this, Sign.class);
+                    startActivity(i);
+                 }else if(professor.isChecked()){
+                    Intent i = new Intent(MainActivity.this, sign_professor.class);
+                    startActivity(i);
+                }
+            }
+        });
+
+        //로그인 버튼 정의
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if(stdNum.getText().toString().equals("") ||
+                        pw.getText().toString().equals("")) {
+                    Toast.makeText(MainActivity.this, "입력오류입니다.",Toast.LENGTH_SHORT).show();
+                    return;
+                }else{
+                    //2016-10-18
+                    //로그인 넘어가는 화면 작성
+                }
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/com/example/lym/han1129_first/sign_professor.java
@@ -0,0 +1,44 @@
+package com.example.lym.han1129_first;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+/**
+ * Created by LYM on 2016-10-18.
+ */
+public class sign_professor extends Activity {
+    Button joinOk, joincancel;
+    EditText proPassword, proNumber, proCode,proName;
+    Spinner stdGrade;
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sign_professor);
+        setTitle("교수 회원가입");
+
+        joinOk = (Button) findViewById(R.id.joinOk);
+        joincancel = (Button) findViewById(R.id.joincancel);
+
+        proName = (EditText) findViewById(R.id.proName);
+        proPassword = (EditText) findViewById(R.id.proPassword);
+        proNumber = (EditText) findViewById(R.id.proNumber);
+        proCode = (EditText) findViewById(R.id.proCode);
+
+        //입력 하나라도 빠지면 토스트메세지 전달
+        joinOk.setOnClickListener(new View.OnClickListener(){
+            public  void onClick(View v){
+                if(proName.getText().toString().equals("") ||
+                        proNumber.getText().toString().equals("") ||
+                        proPassword.toString().equals("")){
+                    Toast.makeText(sign_professor.this, "입력오류입니다.",Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+            }
+        });
+    }
+}
